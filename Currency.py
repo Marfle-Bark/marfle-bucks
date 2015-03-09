cb62003330b2a45a648261afd0508b9256da59ce
Marfle-Bark <ethan.busbee@gmail.com>
2015-03-09 06:30:42 -0500

    Rewrote methods to overload native +=*/ operators, rewrote getApproxAmount(), compacted tests.

--- Currency.py
+++ Currency.py
@@ -9,29 +9,29 @@ class Currency(object):
             self._uDollars = int(round(amount * 1000000))
 
     def getExactAmount(self): return self._uDollars
-    def getApproxAmount(self): return float("{:1,.2f}".format(self._uDollars / 1000000.0)) # This is a dirty hack
-    def toString(self): return "$" + "{:1,.2f}".format(self._uDollars / 1000000.0)
+    def getApproxAmount(self): return round(self._uDollars / 1000000.0, 2)
+    def __str__(self): return "$" + "{:1,.2f}".format(self.getApproxAmount())
 
     def getDollarPart(self): return self._uDollars / 1000000
     def getCentPart(self): return (self._uDollars / 10000) % 100
 
-    def add(self, other):
+    def __add__(self, other):
         if type(other) is not type(self):
             other = Currency(other)
-        self._uDollars += other.getExactAmount()
+        return Currency(self.getApproxAmount() + other.getApproxAmount())
 
-    def sub(self, other):
+    def __sub__(self, other):
         if type(other) is not type(self):
             other = Currency(other)
-        self._uDollars -= other.getExactAmount()
+        return Currency(self.getApproxAmount() - other.getApproxAmount())
 
-    def mul(self, factor):
+    def __mul__(self, factor):
         self._uDollars *= factor
-        self._uDollars = int(round(self._uDollars))
+        return Currency(self.getApproxAmount())
 
-    def div(self, factor):
+    def __div__(self, factor):
         self._uDollars /= factor
-        self._uDollars = int(round(self._uDollars))
+        return Currency(self.getApproxAmount())
 
     # Comparisons are defined as approximate: it only checks down to $0.01, NOT micro-dollars!
     def __lt__(self, other): return type(self) == type(other) and self.getApproxAmount() < other.getApproxAmount()
@@ -46,61 +46,44 @@ class Currency(object):
 if __name__ == "__main__":
     print "Note: this class simulates down to micro-dollars to reduce rounding error."
     print "Also, right now this class ONLY handles USD and all output is formatted with regards to that."
-    print "It doesn't yet do conversion of any kind, so you'll need to write your own glue code."
-    print "ALSO COMPARISONS ARE APPROXIMATE"
+    print "It doesn't yet do currency conversion of any kind, so you'll need to write your own glue code."
+    print "ALSO COMPARISONS ARE APPROXIMATE TO ONE PENNY"
     print "Made with <3 by Ethan Busbee for his own learning."
     print ""
 
-    print "***Init to $12.34"
     dough = Currency(12.34)
-    print dough.toString()
+    print "***Init to $12.34: " + str(dough)
 
-    print "***Add $10"
-    dough.add(10)
-    print dough.toString()
+    dough += 10
+    print "***Add $10: " + str(dough)
 
-    print "***Remove $5.00"
-    dough.sub(5.00)
-    print dough.toString()
+    dough -= 5.00
+    print "***Remove $5.00: " + str(dough)
 
-    print "***Multiply by 5x"
-    dough.mul(5)
-    print dough.toString()
+    dough *= 5
+    print "***Multiply by 5x: " + str(dough)
 
-    print "***Divide by 5x"
-    dough.div(5)
-    print dough.toString()
+    dough /= 5
+    print "***Divide by 5x: " + str(dough)
 
     print "***Show dollars and cents separately."
-    print "Dollars: " + str(dough.getDollarPart())
-    print "Cents: " + str(dough.getCentPart())
+    print "Dollars: " + str(dough.getDollarPart()) + ", Cents: " + str(dough.getCentPart())
 
     # Comparison Operators
     cash = Currency(10)
-    money = Currency()
-    money.add(5.50)
-    money.add(5)
-    money.sub(0.50)
+    money = Currency(10.00)
     # cash and money should both be $10.00 at this point
+    print "***Compare $10.00 == $10.00: " + str(cash == money)
+    print "***Compare $10.00 != $10.00: " + str(cash != money)
 
-    print "***Compare $10.00 == $10.00 and $10.00 != $10.01:"
-    print cash == money
-    print cash != money
+    money += 0.01 # make money be slightly higher than cash
+    print "***Compare $10.00 == $10.01: " + str(cash == money)
+    print "***Compare $10.00 != $10.01: " + str(cash != money)
 
-    print "***Compare $10.00 == $10.01 and $10.00 != $10.01:"
-    money.add(0.01) # make money be slightly higher than cash
-    print cash == money
-    print cash != money
+    print "***Compare $10.01 < $10.00: " + str(money < cash)
+    print "***Compare $10.00 < $10.01: " + str(cash < money)
 
-    print "***Compare $10.01 < $10.00:"
-    print money < cash
-
-    print "***Compare $10.00 < $10.00:"
-    print cash < money
-
-    money.sub(0.01)
-    print "***Compare $10.00 <= $10.00:"
-    print cash <= money
-
-    print "***Compare $10.00 >= $10.00"
-    print cash >= money
+    money -= 0.01
+    print "***Compare $10.00 < $10.00: " + str(cash < money)
+    print "***Compare $10.00 <= $10.00: " + str(cash <= money)
+    print "***Compare $10.00 >= $10.00: " + str(cash >= money)
